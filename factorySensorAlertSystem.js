4ead4e1931f805281b0c76f5f5f9abaf8e975f0a
LA4th <lorenzolaforteza311@gmail.com>
2025-07-04 06:28:26 +0800

    Adding the array

---
+++ factorySensorAlertSystem.js
@@ -0,0 +1,11 @@
+const machine = [
+  {id: "M-01", temperature: 75},
+  {id: "M-02", temperature: 83},
+  {id: "M-03", temperature: 78},
+  {id: "M-04", temperature: 95},
+  {id: "M-05", temperature: 67}
+];
+
+const machineChecker = (machine) => {
+  
+};
